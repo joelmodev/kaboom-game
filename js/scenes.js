121567f0848d4319e1058332aabf4bb7b9e736ea
Heitor Juliani <134893946+joelmodev@users.noreply.github.com>
2024-06-30 15:25:52 +0000

    Versão 1.05

--- js/scenes.js
+++ js/scenes.js
@@ -21,6 +21,7 @@ scene('game', () => {
     ])
     // .jump() when "space" key is pressed
     onKeyPress("space", () => {
+        debug.log("Tecla espaço precionada")
         if (bean.isGrounded()) {
             bean.jump(800)
         }
